cb6881de0d793e0b3add4ee416eed6ad465c5b4c
Ray Ruzzo <rrayruzz@gmail.com>
2023-08-03 12:23:54 -0400

    Updated to use API

--- tarot/models.py
+++ tarot/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class tarotCard(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="tarot/cards")
-    meaning = models.TextField(max_length=2000)
-    reversed_meaning = models.TextField(max_length=2000)
+class reading(models.Model):
+    user = models.CharField(max_length=100)
+    question = models.CharField(max_length=500)
+    cards = models.CharField(max_length=500)
 
     def __str__(self):
-        return self.name
-
+        return self.user + " - " + self.question + " - " + self.cards
--- tarot/views.py
+++ tarot/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import tarotCard
 from . import forms
 import random
+import requests
 
 # Create your views here.
 def randomCard(request):
@@ -13,17 +14,18 @@ def randomCard(request):
     return render(request, 'tarot/random_card.html', context={'card':card})
 
 def pastPresentFuture(request):
-    queryset = tarotCard.objects.all()
-    names = [card.name for card in queryset]
-    context = {}
-    context['name'] = request.POST.get('name')
-    context['question'] = request.POST.get('question')
-    cards_drawn = random.sample(range(0,len(names)),3)
-    positions = ['past','present','future']
-    for card,position in zip(cards_drawn,positions):
-        context[position] = tarotCard.objects.get(name=names[card])
-        context[position+"_reversed"] = round(random.random())
-        
+    cards = requests.get("https://tarot-api-3hv5.onrender.com/api/v1/cards/random?n=3")
+    cards = cards.json()
+    context = {"past": cards["cards"][0],
+                   "present": cards["cards"][1], 
+                   "future": cards["cards"][2]}
+    context['past_reversed'] = random.randint(0,1)
+    context['present_reversed'] = random.randint(0,1)
+    context['future_reversed'] = random.randint(0,1)
+    context['past']['image'] = f"/media/tarot/cards/{context['past']['name'].replace(' ','_')}.jpg"
+    context['present']['image'] = f"/media/tarot/cards/{context['present']['name'].replace(' ','_')}.jpg"
+    context['future']['image'] = f"/media/tarot/cards/{context['future']['name'].replace(' ','_')}.jpg"
+
     return render(request, 'tarot/past_present_future.html', context=context)
 
 def home(request):
