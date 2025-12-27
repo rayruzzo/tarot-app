8f91f41272ca71a38d4ca81b56de6789fccab13c
Ray Ruzzo <rrayruzz@gmail.com>
2025-12-27 14:00:37 -0500

    updating media url

--- tarot_project/settings.py
+++ tarot_project/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_URL = "/media/"
+MEDIA_URL = "https://res.cloudinary.com/dljouwgcl/image/upload/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
